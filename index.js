41d811b17752a95939ae47322ade66ebc434ee7c
cesarmouradeveloper@gmail.com <EXPEDITO0579@DIGITALCOLLEGE.LOCAL>
2024-08-06 10:47:34 -0300

    update

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
 const express = require('express')
-var cors = requeri('cors')
+var cors = require('cors')
 const app = express()
-const porta = 1000
+const port = 10000
 
 app.use(cors())
 
-app.get('/', (resq, res)) => {
-    console.log('resquest.url', request.url)//debug
-    console.log('request.params.id', resquest.params.id)
+app.get('/', (req, res)=>{
+    res.send('Olá, mundo')
+})
 
-    //COMMECO DO CODIGO BRABO
+
+// https://pagamento.inter.com/pagamento?tipo=boleto&valor=100&pedido=100&desconto
+
+app.get('/v1/user/:id', (request, res) => {
+    console.log('request.url', request.url) // debug
+    console.log('request.params.id', request.params.id)
+
+    // COMECO DO CODIGO BRABO
     const mysql = require('mysql2');
 
-    //Conecte-se conexão com o bancos de doas
+    // Crie uma conexão com o banco de dados
     const connection = mysql.createConnection({
-            host:'localhost',
-            user: 'root',
-            password:'',
-            database: 'dc'
-        });
-}
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'dc'
+    });
 
-// Conecte-se ao banco de dados
-connection.connect((err) => {
-    if (err) {
-        return console.error('Erro ao conectar: ' + err.stack);
-    }
-    console.log('Conecte com id ' + connetion.threadId);
+    // Conecte-se ao banco de dados
+    connection.connect((err) => {
+        if (err) {
+            return console.error('Erro ao conectar: ' + err.stack);
+        }
+        console.log('Conectado como id ' + connection.threadId);
+
+
+        // Execute uma consulta SQL
+        connection.query('select * from dc.usuarios where id='+request.params.id+';', (err, results, fields) => {
+            if (err) {
+                console.error('Erro ao executar consulta: ' + err.stack);
+                return;
+            }
+            console.log('Resultados da consulta:', results);
+            res.send(results)
+        });
+
+    });
+
+
+    // FIM DO CODIGO BRABO
+    // 'select * from dc.usuarios where id=1;'
+    // res.send({
+    //     "id": 1,
+    //     firstname: "primeiro nome",
+    //     surname: 'segundo nome',
+    //     email: 'email@email.com',
+    //     password: 1234
+    // })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
